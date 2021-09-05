49ff55a7b0c766bed0975011eb9a49e295cf6a2f
imerofeev <imerofeev@gmail.com>
2021-09-05 21:16:36 +0300

    feat: guessing game solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min = 0
+    this.max = 0
+    this.middle = 0
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min
+    this.max = max
+  }
 
-    }
+  guess() {
+    this.middle = Math.ceil((this.max + this.min) / 2)
+    return this.middle
+  }
 
-    guess() {
+  lower() {
+    this.max = this.middle
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.middle
+  }
 }
 
 module.exports = GuessingGame;
